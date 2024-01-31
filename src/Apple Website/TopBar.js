80f580442a14342a9191fe4e01cb17c07b386275
hasim24 <erhashim24@gmail.com>
2024-01-31 12:23:40 +0530

    deploy

--- src/Apple Website/TopBar.js
+++ src/Apple Website/TopBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 // import AppleIcon from "@mui/icons-material/Apple";
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import appleIcon from './images/appleicon.png'
 
 
 const TopBar = () => {
@@ -15,6 +16,7 @@ const TopBar = () => {
                  height:"80px",
                  marginBottom:"10px"}}>
             {/* <AppleIcon style={{paddingRight:"10px"}} /> */}
+            <img style={{width:"35px",height:"35px",backgroundColor:"white"}} src={appleIcon}/>
         {links.map((link,index) => (
             <div>
                 <a key={index} style={{color:"white"}}>{link}</a>
